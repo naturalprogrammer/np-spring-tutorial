efd0663ab960409761c1fae7fcd6a7b6b06b94de
Sanjay <skpatel20@gmail.com>
2016-02-16 13:51:04 +0530

    New project created

---
+++ src/main/java/com/naturalprogrammer/spring/tutorial/NpSpringTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpSpringTutorialApplication.class, args);
+	}
+}
