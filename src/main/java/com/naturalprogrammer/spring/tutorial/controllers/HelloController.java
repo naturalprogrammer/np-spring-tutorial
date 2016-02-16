161d0c3ebfa23d7388931e72db24649b39962b73
Sanjay <skpatel20@gmail.com>
2016-02-16 13:56:08 +0530

    Hello, world

---
+++ src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.spring.tutorial.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello, world";
+	}
+}
