33a8f714e6464def437ea839948ecd850bc4c6d4
Sanjay <skpatel20@gmail.com>
2016-02-21 13:32:13 +0530

    Conditional annotations

--- src/main/java/com/naturalprogrammer/spring/tutorial/controllers/MailController.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/controllers/MailController.java
@@ -1,7 +1,6 @@
 package com.naturalprogrammer.spring.tutorial.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/naturalprogrammer/spring/tutorial/mail/MailConfig.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/mail/MailConfig.java
@@ -1,20 +1,22 @@
 package com.naturalprogrammer.spring.tutorial.mail;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MailConfig {
 	
 	@Bean
-	@Profile("dev")
+	@ConditionalOnProperty(name="spring.mail.host",
+		havingValue="foo",
+		matchIfMissing=true)
 	public MailSender mockMailSender() {
 		return new MockMailSender();
 	}
 
 	@Bean
-	@Profile("!dev")
+	@ConditionalOnProperty(name="spring.mail.host")
 	public MailSender smtpMailSender() {
 		return new SmtpMailSender();
 	}
