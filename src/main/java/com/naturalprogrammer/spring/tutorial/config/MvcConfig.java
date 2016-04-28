4866b7f77a18ea0f265ee8b2aa162b4ceb3f07fc
Sanjay <skpatel20@gmail.com>
2016-04-28 18:16:17 +0530

    MVC Basics 2

---
+++ src/main/java/com/naturalprogrammer/spring/tutorial/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.naturalprogrammer.spring.tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
--- src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HomeController.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HomeController.java
@@ -1,13 +1,13 @@
-package com.naturalprogrammer.spring.tutorial.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class HomeController {
-
-	@RequestMapping("/")
-	public String home() {
-		return "home";
-	}
-}
+//package com.naturalprogrammer.spring.tutorial.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//	@RequestMapping("/")
+//	public String home() {
+//		return "home";
+//	}
+//}
