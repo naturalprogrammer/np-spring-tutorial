03edd7c565285b4c032587c9ebbbbcb4d246543a
Sanjay <skpatel20@gmail.com>
2016-05-01 12:52:25 +0530

    I18n, before getMessage utility method

--- src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HelloController.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package com.naturalprogrammer.spring.tutorial.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
+    @Autowired
+    private MessageSource messageSource;
+
+    @RequestMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("name", "Sanjay");
-		return "hello";
+    	
+    	model.addAttribute("name",
+    		messageSource.getMessage("name", null,
+    			LocaleContextHolder.getLocale()));
+
+    	return "hello";
 	}
 }
