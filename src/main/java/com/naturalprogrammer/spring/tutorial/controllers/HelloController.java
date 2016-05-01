45ed8fd3f1e928db0c2c8f6ee2591d0b46d05ae3
Sanjay <skpatel20@gmail.com>
2016-05-01 17:51:28 +0530

    Coded path variable and request param demo

--- src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HelloController.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/controllers/HelloController.java
@@ -1,19 +1,24 @@
 package com.naturalprogrammer.spring.tutorial.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.naturalprogrammer.spring.tutorial.util.MyUtil;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-	public String hello(Model model) {
-    	
-    	model.addAttribute("name", MyUtil.getMessage("name"));
+	@RequestMapping("/hello/{id}")
+	public String hello(Model model,
+		@PathVariable String id,
+		@RequestParam Optional<String> name) {
+		
+		model.addAttribute("id", id);	
+		model.addAttribute("name", name.orElse("No Name"));	
 
-    	return "hello";
+		return "hello";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
 <%@include file="includes/header.jsp"%> 
 
-<spring:message code="hello" />, ${name}!
+<spring:message code="hello" />, ${name}! Your Id is ${id}.
 
 <%@include file="includes/footer.jsp"%>
