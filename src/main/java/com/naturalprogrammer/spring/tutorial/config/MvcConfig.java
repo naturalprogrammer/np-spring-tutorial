892c1f504117fb3b1cbddea23fce301b759c6340
Sanjay <skpatel20@gmail.com>
2016-05-02 09:12:41 +0530

    Different ways to customize errors

--- src/main/java/com/naturalprogrammer/spring/tutorial/config/MvcConfig.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/config/MvcConfig.java
@@ -1,6 +1,10 @@
 package com.naturalprogrammer.spring.tutorial.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,5 +14,15 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Autowired
+    private MessageSource messageSource;
+    
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        factory.setValidationMessageSource(messageSource);
+        return factory;
     }
 }
