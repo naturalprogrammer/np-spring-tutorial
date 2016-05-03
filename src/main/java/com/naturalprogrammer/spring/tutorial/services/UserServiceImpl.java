8acab75c294f8b96e97ea5974c919e1baa7876b3
Sanjay <skpatel20@gmail.com>
2016-05-03 10:17:23 +0530

    Coded @Transactional

--- src/main/java/com/naturalprogrammer/spring/tutorial/services/UserServiceImpl.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/services/UserServiceImpl.java
@@ -2,18 +2,22 @@ package com.naturalprogrammer.spring.tutorial.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.naturalprogrammer.spring.tutorial.domain.User;
 import com.naturalprogrammer.spring.tutorial.domain.User.Role;
 import com.naturalprogrammer.spring.tutorial.repositories.UserRepository;
 
 @Service
+@Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void signup(User user) {
 
 		user.getRoles().add(Role.UNVERIFIED);
