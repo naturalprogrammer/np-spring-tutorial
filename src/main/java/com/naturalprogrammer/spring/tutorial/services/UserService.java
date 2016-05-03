24572fee9648a347479a4d8f27d8fb66117903cb
Sanjay <skpatel20@gmail.com>
2016-05-03 11:08:19 +0530

    Coded startup code

--- src/main/java/com/naturalprogrammer/spring/tutorial/services/UserService.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/services/UserService.java
@@ -1,8 +1,11 @@
 package com.naturalprogrammer.spring.tutorial.services;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
 import com.naturalprogrammer.spring.tutorial.domain.User;
 
 public interface UserService {
 
 	void signup(User user);
+	void afterApplicationReady(ApplicationReadyEvent event);
 }
--- src/main/java/com/naturalprogrammer/spring/tutorial/services/UserServiceImpl.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/services/UserServiceImpl.java
@@ -1,6 +1,13 @@
 package com.naturalprogrammer.spring.tutorial.services;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +20,44 @@ import com.naturalprogrammer.spring.tutorial.repositories.UserRepository;
 @Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
 public class UserServiceImpl implements UserService {
 
+	private static final Log log = LogFactory.getLog(UserServiceImpl.class);
+
+	@PostConstruct
+	public void postConstruct() {
+		log.info("UserServiceImpl constructed");
+	}
+
 	@Autowired
 	private UserRepository userRepository;
+	
+    @Value("${app.admin.email:admin@example.com}")
+    private String adminEmail;
+	
+    @Value("${app.admin.name:First Admin}")
+    private String adminName;
 
+    @Value("${app.admin.password:password}")
+    private String adminPassword;
+
+    @Override
+	@EventListener
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
+	public void afterApplicationReady(ApplicationReadyEvent event) {
+
+		if (!userRepository.findByEmail(adminEmail).isPresent()) {
+
+			User user = new User();
+
+			user.setEmail(adminEmail);
+			user.setName(adminName);
+			user.setPassword(adminPassword);
+
+			user.getRoles().add(Role.ADMIN);
+
+			userRepository.save(user);			
+		}		
+	}
+	
 	@Override
 	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void signup(User user) {
