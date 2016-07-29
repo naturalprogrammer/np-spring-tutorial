81a5046c247f02c6e52bdac8d2d05b41fea01560
Sanjay <skpatel20@gmail.com>
2016-07-29 20:24:20 +0530

    New project with Spring Boot 1.4

--- src/test/java/com/naturalprogrammer/spring/tutorial/NpSpringTutorialApplicationTests.java
+++ src/test/java/com/naturalprogrammer/spring/tutorial/NpSpringTutorialApplicationTests.java
@@ -2,10 +2,7 @@ package com.naturalprogrammer.spring.tutorial;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
