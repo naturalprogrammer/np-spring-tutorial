fb032499282b09c561216e3466a835e40f39e39c
Sanjay <skpatel20@gmail.com>
2016-07-31 12:05:16 +0530

    Updated for Spring Boot 1.4

--- src/main/java/com/naturalprogrammer/spring/tutorial/config/SecurityConfig.java
+++ src/main/java/com/naturalprogrammer/spring/tutorial/config/SecurityConfig.java
@@ -1,24 +1,11 @@
 package com.naturalprogrammer.spring.tutorial.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder builder)
-			throws Exception {
-
-		builder.userDetailsService(userDetailsService);
-	}
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
